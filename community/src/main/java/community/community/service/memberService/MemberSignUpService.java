613b5c98b33f5397da2b3b10b7a176af6f37922c
hyeon4869 <119660070+hyeon4869@users.noreply.github.com>
2024-10-05 20:05:15 +0900

    회원가입 로직 예외처리 ì일부 추가 및 읽기전용쿼리 사용

--- community/src/main/java/community/community/service/memberService/MemberSignUpService.java
+++ community/src/main/java/community/community/service/memberService/MemberSignUpService.java
@@ -2,11 +2,14 @@ package community.community.service.memberService;
 
 import community.community.dto.MemberDTO.MemberDTO;
 import community.community.entity.Member;
-import community.community.exception.customException.DuplicateEmailException;
+import community.community.exception.customException.DBAccessException;
+import community.community.exception.customException.InvalidEmailException;
+import community.community.exception.customException.SignUpFailedException;
 import community.community.interfaceService.memberInterface.MemberSignUpInterface;
 import community.community.repository.MemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.dao.DataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -38,20 +41,30 @@ public class MemberSignUpService implements MemberSignUpInterface {
         if(memberDTO.getEmail()==null || memberDTO.getPassword()==null){
             throw new IllegalArgumentException("아이디나 비밀번호는 필수입니다.");
         }
-        Member member = Member.builder()
-                .email(memberDTO.getEmail())
-                .password(memberDTO.getPassword())
-                .build();
-        memberRepository.save(member);
+        try {
+            Member member = Member.builder()
+                    .email(memberDTO.getEmail())
+                    .password(memberDTO.getPassword())
+                    .build();
+            memberRepository.save(member);
 
-        return String.valueOf(member.getId());
+            return String.valueOf(member.getId());
+
+        } catch (DataAccessException e) {
+            throw new DBAccessException("서버의 오류로 회원가입에 실패했습니다", e);
+        } catch (Exception e){
+            throw new SignUpFailedException("서버의 오류로 회원가입에 실패했습니다.", e);
+        }
+
     }
 
     @Override
     public void validationDuplicateEmail(String email){
-
+        if(email==null||email.trim().isEmpty()){
+            throw new IllegalArgumentException("조회할 이메일이 null 입니다.");
+        }
         if(memberRepository.findByReadEmail(email).isPresent()){
-            throw new DuplicateEmailException("이미 사용중인 이메일입니다.");
+            throw new InvalidEmailException("이미 사용중인 이메일입니다.");
         }
     }
 }
