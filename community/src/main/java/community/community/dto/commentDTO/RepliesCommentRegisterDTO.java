0a1cd9eace61faef71b90ba1c8dc04ec76c66211
hyeon4869 <119660070+hyeon4869@users.noreply.github.com>
2025-03-10 21:37:49 +0900

    조회 기능 완료

--- community/src/main/java/community/community/dto/commentDTO/RepliesCommentRegisterDTO.java
+++ community/src/main/java/community/community/dto/commentDTO/RepliesCommentRegisterDTO.java
@@ -1,14 +1,15 @@
 package community.community.dto.commentDTO;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import community.community.entity.Comment;
+import lombok.*;
+
+import java.util.Date;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class RepliesCommentRegisterDTO {
 
     private Long id;
@@ -16,5 +17,15 @@ public class RepliesCommentRegisterDTO {
     private Long parentCommentId;
 
     private String content;
+
+    private Date createTime;
+
+    public static RepliesCommentRegisterDTO fromEntity(Comment comment){
+        RepliesCommentRegisterDTO repliesCommentRegisterDTO = RepliesCommentRegisterDTO.builder()
+                .content(comment.getContent())
+                .createTime(comment.getCreateTime())
+                .build();
+        return repliesCommentRegisterDTO;
+    }
 
 }
--- community/src/main/java/community/community/repository/commentRepository/CommentRepository.java
+++ community/src/main/java/community/community/repository/commentRepository/CommentRepository.java
@@ -16,4 +16,8 @@ public interface CommentRepository extends JpaRepository<Comment, Long> {
     @Query("SELECT c FROM Comment c JOIN FETCH  c.post p where p.id=:id AND c.parentComment IS NULL")
     @QueryHints(@QueryHint(name = "org.hibernate.readOnly", value = "true"))
     List<Comment> findReadAll(@Param("id") Long id);
+
+    @Query("SELECT c FROM Comment c WHERE c.parentComment.id = :parentId ORDER BY createTime ")
+    @QueryHints(@QueryHint(name = "org.hibernate.readOnly", value = "true"))
+    List<Comment> findRepliesReadAll(@Param("parentId") Long parentId);
 }
